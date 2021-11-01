9c4b26b9e578fdf74b06ba92f63fcf4ec076bf06
Blake McCarty <blake_mccarty@yahoo.com>
2021-11-01 18:55:20 -0500

    asteroids

--- p5.js
+++ p5.js
@@ -1,5 +1,6 @@
 var ship;
 var asteroids = [];
+var lasers = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,22 +12,36 @@ function setup() {
 
 function draw() {
   background(0);
-  ship.render();
-  ship.turn();
-  ship.update();
-  ship.edges();
+
   asteroids.forEach((asteroid) => {
     asteroid.render();
     asteroid.update();
     asteroid.edges();
   });
+  lasers.forEach((laser) => {
+    laser.render();
+    laser.update();
+    asteroids.forEach((asteroid) => {
+      if (laser.hits(asteroid)) {
+        var newAsteroids = asteroid.breakup();
+        asteroids.push(newAsteroids);
+        asteroids.splice(asteroid, 1);
+      }
+    });
+  });
+  ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
 }
 
 function keyReleased() {
   ship.setRotation(0);
 }
 function keyPressed() {
-  if (keyCode == RIGHT_ARROW) {
+  if (key == ' ') {
+    lasers.push(new Laser(ship.pos, ship.heading));
+  } else if (keyCode == RIGHT_ARROW) {
     ship.setRotation(0.1);
   } else if (keyCode == LEFT_ARROW) {
     ship.setRotation(-0.1);
--- ship.js
+++ ship.js
@@ -25,9 +25,9 @@ function Ship() {
 
   this.render = function () {
     push();
-    translate(this.pos.x, this.pos.y);
+    translate(this.pos.x, this.pos.y); 
     rotate(this.heading + PI / 2);
-    noFill();
+    fill(0);
     stroke(255);
     triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
     pop();
